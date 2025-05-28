e8faea240887772574fef8c7bb435b724713c51d
hsucoder <hsuthazin255781@gmail.com>
2025-05-28 12:44:14 +0630

    Add

--- api_security/api_security/wsgi.py
+++ api_security/api_security/wsgi.py
@@ -15,5 +15,5 @@ sys.path.append(BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_security.settings')
 application = get_wsgi_application()
