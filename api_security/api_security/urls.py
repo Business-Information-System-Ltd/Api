81c798b30d016b37c527cbb05dd901115a479c60
hsucoder <hsuthazin255781@gmail.com>
2025-06-02 13:32:07 +0630

    update url

--- api_security/api_security/urls.py
+++ api_security/api_security/urls.py
@@ -26,7 +26,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('security.urls')),
-    path('api/token/', TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/token/', csrf_exempt(TokenObtainPairView.as_view()),name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),name='token_refresh'),
     path('api/protected/', protected_view, name='protected'),
 
--- api_security/security/urls.py
+++ api_security/security/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import profile_view, BudgetViewSet
+from django.http import HttpResponse
+
+
+
+def oauth_callback(request):
+    return HttpResponse("OK! You are Successfully. OAuth callback handled")
 
 router = DefaultRouter()
 router.register(r'budget', BudgetViewSet, basename='budget')
+
+
+
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('', include(router.urls)),  
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('oauth/callback/', oauth_callback),
 ]
